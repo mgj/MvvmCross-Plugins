5eda1776939d9d1384846a2e15afe94f5a1a713f
Mikkel Jensen <mgj@artm.dk>
2017-02-24 13:04:08 +0100

    wip

--- Dialog/artm.MvxPlugins.Dialog.Touch/Services/MultiChoiceViewController.cs
+++ Dialog/artm.MvxPlugins.Dialog.Touch/Services/MultiChoiceViewController.cs
@@ -17,23 +17,16 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
         private readonly IEnumerable _items;
         private MvxTableViewSource _source;
         private UITableView _table;
-        private readonly bool[] _checkedItems;
-        private readonly Action<List<int>> _onComplete;
-        private readonly string _okLabel;
-        private readonly string _cancelLabel;
+        private readonly MultiChoiceViewControllerBundle _bundle;
 
-        public MultiChoiceViewController(string title, string okLabel, string cancelLabel, IEnumerable items, bool[] checkedItems, Action<List<int>> onComplete)
+        public MultiChoiceViewController(MultiChoiceViewControllerBundle bundle)
         {
-            if(string.IsNullOrEmpty(title) == false)
+            if(string.IsNullOrEmpty(bundle.Title) == false)
             {
-                this.Title = title;
+                this.Title = bundle.Title;
             }
 
-            _okLabel = okLabel;
-            _cancelLabel = cancelLabel;
-            _items = items;
-            _checkedItems = checkedItems;
-            _onComplete = onComplete;
+            _bundle = bundle;
         }
 
         public override void ViewDidLoad()
@@ -96,16 +89,16 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
 
         private void PrepareTableViewSource()
         {
-            _source = new MyTableViewSource(_table, _checkedItems);
+            _source = new MyTableViewSource(_table, _bundle.CheckedItems);
             _source.ItemsSource = _items;
             _table.Source = _source;
         }
 
         private void PrepareSelectedRowItems()
         {
-            for (int i = 0; i < _checkedItems.Count(); i++)
+            for (int i = 0; i < _bundle.CheckedItems.Count(); i++)
             {
-                var hero = _checkedItems[i];
+                var hero = _bundle.CheckedItems[i];
                 var path = NSIndexPath.Create(0, i); // Section must be supplied. Assuming there is only 1 section.
                 if (hero == true)
                 {
@@ -133,7 +126,6 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
                 var cell = base.GetCell(tableView, indexPath);
 
                 cell.ImageView.Image = UIImage.FromFile("Images/" + tableItems[indexPath.Row].ImageName); // don't use for Value2
-
 
 
                 cell.SelectionStyle = UITableViewCellSelectionStyle.None;
@@ -172,7 +164,7 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
         private UIButton PrepareCancelButton()
         {
             var button = UIButton.FromType(UIButtonType.System);
-            button.SetTitle(_cancelLabel, UIControlState.Normal);
+            button.SetTitle(_bundle.CancelLabel, UIControlState.Normal);
             button.TouchUpInside += (sender, e) =>
             {
                 PrepareSelectedRowItems(); // Reset the table items to whatever was originally passed in
@@ -185,7 +177,7 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
         private UIButton PrepareOkButton()
         {
             var button = UIButton.FromType(UIButtonType.System);
-            button.SetTitle(_okLabel, UIControlState.Normal);
+            button.SetTitle(_bundle.OkLabel, UIControlState.Normal);
             button.TouchUpInside += (sender, e) =>
             {
                 DismissViewController(true, null);
@@ -197,12 +189,12 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
         public override void ViewWillDisappear(bool animated)
         {
             base.ViewWillDisappear(animated);
-            if(_onComplete == null)
+            if(_bundle.OnComplete == null)
             {
                 return;
             }
 
-            _onComplete(GetSelectedRows());
+            _bundle.OnComplete(GetSelectedRows());
         }
 
         public MvxTableViewSource TableSource
--- Dialog/artm.MvxPlugins.Dialog.Touch/Services/MultiChoiceViewControllerBundle.cs
+++ Dialog/artm.MvxPlugins.Dialog.Touch/Services/MultiChoiceViewControllerBundle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,21 @@ namespace artm.MvxPlugins.Dialog.Touch.Services
 {
     public class MultiChoiceViewControllerBundle
     {
+        public MultiChoiceViewControllerBundle(string title, string okLabel, string cancelLabel, IEnumerable items, bool[] checkedItems, Action<List<int>> onComplete)
+        {
+            Title = title;
+            OkLabel = okLabel;
+            CancelLabel = cancelLabel;
+            Items = items;
+            CheckedItems = checkedItems;
+            OnComplete = onComplete;
+        }
+
+        public string CancelLabel { get; private set; }
+        public bool[] CheckedItems { get; private set; }
+        public IEnumerable Items { get; private set; }
+        public string OkLabel { get; private set; }
+        public Action<List<int>> OnComplete { get; private set; }
+        public string Title { get; private set; }
     }
 }
